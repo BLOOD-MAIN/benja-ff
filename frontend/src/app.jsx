bd6c0ace2d90bf07a0cd1e6edde0dcb59e5a2080
Sachithra madusanka <Bloodxmd6@gmail.com>
2025-10-26 16:04:47 +0530

    Add files via upload

---
+++ frontend/src/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import TopUpPage from './pages/TopUpPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/topup" element={<TopUpPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
